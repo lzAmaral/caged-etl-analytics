d9eeb965ab383140cae1f41878ba5281585fb3d0
Luiz Amaral <luizgustavo092005@gmail.com>
2026-07-12 14:40:32 -0300

    chore: setup inicial do projeto Spring Boot

---
+++ src/main/java/br/com/luizamaral/mercado_tech/MercadoTechApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizamaral.mercado_tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoTechApplication.class, args);
+	}
+
+}
